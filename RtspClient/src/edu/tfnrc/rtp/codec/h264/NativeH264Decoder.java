c11071b31a80eb21d4254285c78ffc49e354cce2
PengLei-Adam <ftgh2003@126.com>
2016-01-11 20:10:24 +0800

    Add NativeH264Decoder

---
+++ RtspClient/src/edu/tfnrc/rtp/codec/h264/NativeH264Decoder.java
@@ -0,0 +1,17 @@
+package edu.tfnrc.rtp.codec.h264;
+
+/**
+ * Created by leip on 2016/1/11.
+ */
+public class NativeH264Decoder {
+
+    public static native int initDecoder();
+
+    public static native int DeinitDecoder();
+
+    public static synchronized native int DecodeAndConvert(byte abyte0[], int ai[]);
+
+    public static native int getVideoWidth();
+
+    public static native int getVideoHeight();
+}
